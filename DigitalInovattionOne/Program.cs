f83c7d0a7fd0eb444186418f8035e06960383e82
SoniaBGS <soniabgs@gmail.com>
2021-09-20 18:44:12 -0300

    Adiciona aquivos e inicia alterações

---
+++ DigitalInovattionOne/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DigitalInovattionOne
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
