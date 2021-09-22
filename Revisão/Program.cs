bd67df16b6bc2dad861cdffb645be145373c50da
SoniaBGS <soniabgs@gmail.com>
2021-09-22 01:50:18 -0300

    Adciona todas as modificações

--- Revisão/Program.cs
+++ Revisão/Program.cs
@@ -2,20 +2,57 @@
 
 namespace Revisão
 {
+
     class Program
     {
+
         static void Main(string[] args)
         {
-          Console.WriteLine("Iforme a opção desejada");
+
+           string opcaoUsuario = ObterOpcaoUsuario());
+
+          while (opcaoUsuario.ToUpper() != "X")
+          {
+
+              switch (opcaoUsuario)
+              {
+                case "1";:
+                   //T0DO: adiconar aluno
+                   break;
+                case "2":
+                   //TODO: listar alunes 
+                   break;
+                case "3":
+                //TODO: calcular media geral
+                   break;
+                default:
+                   throw new ArgumentOutOfRangeException();
+           }       
+
+          Console.WriteLine("Informe a opção desejada");
           Console.WriteLine("1- Inserir novo aluno");
-          Console.WriteLine("2- Listar alues");
+          Console.WriteLine("2- Listar alunes");
           Console.WriteLine("3- Calcular média geral");
           Console.WriteLine("X- Sair");
-          Console.WriteLine()
+          Console.WriteLine();
+          
+          opcaoUsuario = ObterOpcaoUsuario();
 
-          string opçãoUsuario
-          )
-           = Console.ReadLine();
+              }
+
+            private static string ObterOpcaoUsuario()
+            {
+
+          Console.WriteLine("Informe a opção desejada");
+          Console.WriteLine("1- Inserir novo aluno");
+          Console.WriteLine("2- Listar alunes");
+          Console.WriteLine("3- Calcular média geral");
+          Console.WriteLine("X- Sair");
+          Console.WriteLine();
+
+          string opcaoUsuario = Console.ReadLine();
+          return opcaoUsuario;
+            }
         }
     }
 }
