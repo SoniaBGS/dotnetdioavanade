15ce2e1b7757a39e76f894619872849db5247639
SoniaBGS <soniabgs@gmail.com>
2021-09-21 23:39:22 -0300

    Adcioa pasta e arquivos, inicia alterações

---
+++ Revisão/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Revisão
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+          Console.WriteLine("Iforme a opção desejada");
+          Console.WriteLine("1- Inserir novo aluno");
+          Console.WriteLine("2- Listar alues");
+          Console.WriteLine("3- Calcular média geral");
+          Console.WriteLine("X- Sair");
+          Console.WriteLine()
+
+          string opçãoUsuario
+          )
+           = Console.ReadLine();
+        }
+    }
+}
+
